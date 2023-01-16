2d92bb2a51ee13fe1bc564f48956635d7380d95e
Sergei <104501886+shmelevsergei@users.noreply.github.com>
2023-01-16 16:41:02 +0300

    Create license.interceptors.ts

---
+++ license.interceptors.ts
@@ -0,0 +1,29 @@
+import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
+import { Router } from "@angular/router";
+import { map } from "rxjs/operators";
+import { CookieService } from "ngx-cookie-service";
+
+@Injectable()
+export class LicenseInterceptor implements HttpInterceptor {
+
+   constructor(
+     protected router: Router,
+     protected cookieService: CookieService
+   ) {}
+
+   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+      const cloned = req.clone({ withCredentials: true });
+      return next.handle(cloned)
+        .pipe(map((event: any) => {
+          const cookieExists: boolean = this.cookieService.check('X-Licensed');
+          if (!cookieExists) {
+            console.error("Invalid license!");
+            console.error("Navigating to error page!");
+            this.router.navigate(["invalid-license"]).then();
+          }
+          return event;
+        }));
+   }
+}
